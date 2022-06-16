a453899cbd199cfadcda114957d2c7866fd395bd
KristinaGalche <hit-tb@mail.ru>
2022-06-16 22:39:52 +1000

    initial

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -1,34 +1,26 @@
 package ru.netology;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class PostmanTest {
     @Test
-    void shouldReturn() {
-        Map<String, String> car = new HashMap<>();
-        car.put("name", "Toyo");
-        car.put("phone", "123");
-        car.put("colour", "red");
-
+    public void shouldReturn() {
+// Given - When - Then
         given()
                 .baseUri("https://postman-echo.com")
-                .body(car) // отправляемые данные (заголовки и query можно выставлять аналогично))
+                .body("Peter") // отправляемые данные (заголовки и query можно выставлять аналогично))
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("[0].name",equalTo("Toyo"))
+                .body("data", equalTo("Peter"))
         ;
-
     }
-
 }
